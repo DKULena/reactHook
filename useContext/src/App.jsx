a6de646be8bec6875a5084bbe0845444fc62281b
DKULena <32173057@dankook.ac.kr>
2025-01-11 18:54:07 +0900

    initial commit

---
+++ useContext/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from './components/Profile'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Profile />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ useContext/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext';
+
+const Footer = () => {
+
+  // required to consuming the context
+  const {phone} = useContext(AppContext);
+
+  return (
+    <div>
+      <h2>Footer</h2>
+      <h3>Phone: {phone}</h3>
+    </div>
+  )
+}
+
+export default Footer
